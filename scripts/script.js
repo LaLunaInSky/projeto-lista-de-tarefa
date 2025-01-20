ed9d398135a4bdfeaf0935683318171d45644eab
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-20 16:14:09 -0300

    20/01/2025 16:14

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,10 @@
 const barraMenuTarefasAdicionadas = document.querySelector("aside")
 const registroAlteraçãoTarefas = document.querySelector(".registro_AlteraçãoTarefas")
 const menuComTarefasAdicionadas = document.querySelector(".menuComTarefasAdicionadas")
+const H1menuComTarefasAdicionadas = document.querySelector(".menuComTarefasAdicionadas>h1")
+const tarefasAdicionadas = document.querySelector(".tarefasAdicionadas")
 const btnSalvar = document.querySelector("#btnSalvar")
-let tarefasAdicionadas = []
+let listaTarefasAdicionadas = []
 
 barraMenuTarefasAdicionadas.addEventListener("click", ()=>{
     if (menuComTarefasAdicionadas.style.display != "flex") {
@@ -32,6 +34,8 @@ class ModeloTarefa{
         this.prioridade = prioridade
         this.descrição = descrição
         this.concluido = false
+
+        criarDesignerTarefa(this.nome)
     }
 
     obterDataAtual = ()=>{
@@ -43,6 +47,35 @@ class ModeloTarefa{
     info = ()=>{
         console.log(`nome da tarefa: ${this.nome}\ndata de conclusão: ${this.dataDeConclusão}\ndata de criação: ${this.dataDeCriação}\nprioridade: ${this.prioridade}\ndescrição: ${this.descrição}\nconcluido: ${this.concluido ? "Sim" : 'Não'}\n`)
     }
+}
+
+const criarDesignerTarefa = (nome)=>{
+    const novaTarefa = document.createElement("article")
+    novaTarefa.setAttribute('id', `tarefa${tarefasAdicionadas.children.length+1 > 10 ? (tarefasAdicionadas.children.length+1) : "0" + (tarefasAdicionadas.children.length+1)}`)
+    novaTarefa.innerHTML = `<div class="icone conclusão">
+                                <abbr title="não concluida">
+                                    <img src="imagens/icone_não_concluido.png" alt="icone conclusão">
+                                </abbr>
+                            </div>
+                            <h2>${nome}</h2>
+                            <div>
+                                <div class="icone">
+                                    <abbr title="visualizar tarefa">
+                                        <img src="imagens/icone_info.png" alt="icone info">
+                                    </abbr>
+                                </div>
+                                <div class="icone">
+                                    <abbr title="editar tarefa">
+                                        <img src="imagens/icone_editar.png" alt="icone editar">
+                                    </abbr>
+                                </div>
+                                <div class="icone">
+                                    <abbr title="deletar tarefa">
+                                        <img src="imagens/icone_deletar.png" alt="icone_deletar">
+                                    </abbr>
+                                </div>
+                            </div>`
+    tarefasAdicionadas.appendChild(novaTarefa)
 }
 
 const verificarInputs = ()=>{
@@ -72,11 +105,15 @@ btnSalvar.addEventListener("click", ()=>{
     if (infoTarefa == undefined) {
         alert('Informações sobre a nova tarefa incompleta!')
     } else {
-        tarefasAdicionadas.push(
+        listaTarefasAdicionadas.push(
             new ModeloTarefa(infoTarefa[0], infoTarefa[1], infoTarefa[2], infoTarefa[3])
         )
     }
 
-    console.log(tarefasAdicionadas)
-})
+    if (listaTarefasAdicionadas.length > 0) {
+        H1menuComTarefasAdicionadas.style.display ='none'
+        tarefasAdicionadas.style.display = 'flex'
+    }
 
+    console.log(listaTarefasAdicionadas)
+})
