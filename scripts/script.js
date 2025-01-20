e36b03f7886da66e0a637fdda0633c43cb8eb45a
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-20 12:24:58 -0300

    20/01/2025 12:24

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 "use strict"
+
+const barraMenuTarefasAdicionadas = document.querySelector("aside")
+const registroAlteraçãoTarefas = document.querySelector(".registro_AlteraçãoTarefas")
+const menuComTarefasAdicionadas = document.querySelector(".menuComTarefasAdicionadas")
+
+barraMenuTarefasAdicionadas.addEventListener("click", ()=>{
+    if (menuComTarefasAdicionadas.style.display != "block") {
+        registroAlteraçãoTarefas.style.display = 'none'
+        menuComTarefasAdicionadas.style.display = 'block'
+    } else {
+        registroAlteraçãoTarefas.style.display = 'block'
+        menuComTarefasAdicionadas.style.display = 'none'
+    }
+    
+})
